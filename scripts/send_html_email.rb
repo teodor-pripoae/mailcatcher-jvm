81974c54e289dfb3f2a33a5ed7ffa715635fddc4
Teodor <teodor.pripoae@gmail.com>
2023-11-20 03:03:38 +0200

    feat: add scripts to send text/html test emails

---
+++ scripts/send_html_email.rb
@@ -0,0 +1,22 @@
+require "mail"
+
+FROM_ADDRESS = "john@example.com"
+FROM = "John Doe <#{FROM_ADDRESS}>"
+TO = "test@example.com"
+
+mail = Mail.deliver do
+  to TO
+  from FROM
+  subject "First multipart email sent with Mail"
+
+  text_part do
+    body "This is plain text"
+  end
+
+  html_part do
+    content_type "text/html; charset=UTF-8"
+    body "<h1>This is HTML</h1>"
+  end
+end
+
+Net::SMTP.start("localhost", 3025) { |smtp| smtp.send_message mail.to_s, FROM_ADDRESS, TO }
---
+++ scripts/send_text_email.rb
@@ -0,0 +1,11 @@
+require "net/smtp"
+
+message = <<MESSAGE_END
+From: Private Person <me@fromdomain.com>
+To: A Test User <test@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+MESSAGE_END
+
+Net::SMTP.start('localhost', 3025){ |smtp|   smtp.send_message message, 'me@fromdomain.com', 'test@todomain.com'}
